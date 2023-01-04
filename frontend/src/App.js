3172d9d6ad4aa6b7c1ec01c1913b8aef24dce518
arjune <arjuneshankar6@gmail.com>
2023-01-04 17:53:38 +0530

    firstcommit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">AJ Clothing Store </a>
+      </header>
+
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
